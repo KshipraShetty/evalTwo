4b6fe1819fc5835ccf30c47dfd60c0ec7e8db561
kshiprashetty <kshiprashetty@gmail.com>
2018-01-23 22:48:29 +0530

    commiting test cases

--- functional_js/filter.js
+++ functional_js/filter.js
@@ -7,10 +7,25 @@ function getShortMessages(arrayMessages) {
 }
 
 module.exports = getShortMessages;
-const arrayObj = [{ message: 'hello there!' },
+
+
+let arrayObj = [{ message: 'hello there!' },
   { message: 'Whats up' },
   { message: 'dfghjkfyghouxhusxhusxuosbsu xbsuxbu qwertyuiopasdfghjkl    jkokzijiohxousxhugxugwux' },
 ];
-const output = ['hello there!', 'Whats up'];
+let output = ['hello there!', 'Whats up'];
 
 console.log('Testing the base case :', getShortMessages(arrayObj).toString() === output.toString());
+
+arrayObj = [{ message: '' },
+  { message: 'hi' }];
+output = ['', 'hi'];
+console.log('Testing the empty message value case :', getShortMessages(arrayObj).toString() === output.toString());
+
+arrayObj = [{ message: 'qwertyuiowertyuisdfghjklbjbj uuxuxuu gugu ijisjnnxknxjnxjxjxnj jbjbsbxhbxsxs' }];
+output = ['', 'hi'];
+console.log('Testing the message value all of which has more than 50 chars case :', getShortMessages(arrayObj).toString() === [].toString());
+
+
+arrayObj = [];
+console.log('Testing the message value all of which has more than 50 chars case :', getShortMessages(arrayObj).toString() === [].toString());
