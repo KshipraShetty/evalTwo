c0f6691741a0f594aa4ac63ba8875ce9c73c6a6c
kshiprashetty <kshiprashetty@gmail.com>
2018-01-24 02:59:45 +0530

    async

---
+++ functional_js/async.js
@@ -0,0 +1,15 @@
+
+function bingo() {
+  const users = [];
+  for (let i = 0; i < 5; i += 1) {
+    users.push(setTimeout(() =>
+      // const a = 1;
+      fun()));
+  }
+  return users;
+}
+
+function fun() {
+  return 1;
+}
+console.log(bingo());
