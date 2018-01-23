de0d041eecf46d7949813e5d39fb98b1b8d4570d
kshiprashetty <kshiprashetty@gmail.com>
2018-01-23 22:25:46 +0530

    other cases

--- functional_js/map.js
+++ functional_js/map.js
@@ -1,8 +1,12 @@
 function doubleAll(numArray) {
-  return numArray.map(arrayNum => arrayNum * 2);
+  if (typeof numArray === typeof []) {
+    return numArray.map(arrayNum => arrayNum * 2);
+  }
+  return false;
 }
 
 module.exports = doubleAll;
 console.log('Should work for base case ', doubleAll([1, 2, 3, 4]).toString() === [2, 4, 6, 8].toString());
 console.log('Should work for negative numbers ', doubleAll([-1, -2, -3, -4]).toString() === [-2, -4, -6, -8].toString());
 console.log('Should not work for empty array ', doubleAll([]).toString() === [].toString());
+console.log('Check for non array elememnts', doubleAll('dfdc') === false);
