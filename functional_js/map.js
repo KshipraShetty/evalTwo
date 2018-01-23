d10f9618d5637c38dffac2bf4761c61764173b10
kshiprashetty <kshiprashetty@gmail.com>
2018-01-23 21:53:01 +0530

    other case

--- functional_js/map.js
+++ functional_js/map.js
@@ -4,3 +4,5 @@ function doubleAll(numArray) {
 
 module.exports = doubleAll;
 console.log('Should work for base case ', doubleAll([1, 2, 3, 4]).toString() === [2, 4, 6, 8].toString());
+console.log('Should work for negative numbers ', doubleAll([-1, -2, -3, -4]).toString() === [-2, -4, -6, -8].toString());
+console.log('Should not work for empty array ', doubleAll([]).toString() === [].toString());
