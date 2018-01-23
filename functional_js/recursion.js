ea20e42d81e37f690a05ccff8426d3f79c17333c
kshiprashetty <kshiprashetty@gmail.com>
2018-01-24 02:27:10 +0530

    test cases

--- functional_js/recursion.js
+++ functional_js/recursion.js
@@ -13,21 +13,20 @@ function countFunction(eleOne, initial) {
 module.exports = reduce;
 
 // base case
-const inputWords = [1, 2, 3];
-const initial = 0;
+let inputWords = [1, 2, 3];
+let initial = 0;
 
 console.log('The base case is: ', reduce(inputWords, countFunction, initial) === 6);
 
 // empty case
-/* inputWords = []
-initial = 0
-console.log('The empty array case is: ', reduce(inputWords, countFunction, initial)===0);
+inputWords = [];
+initial = 0;
+console.log('The empty array case is: ', reduce(inputWords, countFunction, initial) === 0);
 
-inputWords = [1,2,3,4]
+inputWords = [1, 2, 3, 4];
 initial = -5;
-console.log('The negative initial case is: ', reduce(inputWords, countFunction, initial)===5);
+console.log('The negative initial case is: ', reduce(inputWords, countFunction, initial) === 5);
 
-inputWords = [-1,-2,-3,-4]
-initial = 0
-console.log('The negative array case is: ', reduce(inputWords, countFunction, initial)===-10);
-*/
+inputWords = [-1, -2, -3, -4];
+initial = 0;
+console.log('The negative array case is: ', reduce(inputWords, countFunction, initial) === -10);
