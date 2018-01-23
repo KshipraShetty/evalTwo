66b0aaf9c5d4828d4208983dde7e1d83acf9b76f
kshiprashetty <kshiprashetty@gmail.com>
2018-01-23 23:10:14 +0530

    added the extra case

--- functional_js/every_some.js
+++ functional_js/every_some.js
@@ -24,3 +24,47 @@ console.log('Test for base case: ', checkUsersValid(goodUsers) === true);
 goodUsers = [];
 submittedUsers = [];
 console.log('Test for empty id case: ', checkUsersValid(goodUsers) === true);
+
+// no match case
+goodUsers = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+];
+submittedUsers = [
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+];
+console.log('Test for false case: ', checkUsersValid(goodUsers) === false);
+
+goodUsers = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 5 },
+  { id: 6 },
+  { id: 7 },
+];
+submittedUsers = [
+  { id: 2 },
+  { id: 4 },
+];
+console.log('Test for false case: ', checkUsersValid(goodUsers) === false);
+
+goodUsers = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+  { id: 4 },
+  { id: 6 },
+  { id: 7 },
+  { id: 8 },
+  { id: 9 },
+  { id: 5 },
+];
+submittedUsers = [
+  { id: 5 },
+  { id: 9 },
+];
+console.log('Test for longer list of good users containing submitted ids at the end: ', checkUsersValid(goodUsers) === true);
