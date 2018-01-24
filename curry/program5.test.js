8e5b7886d52db2fa8bbd0a172546f58f92e1ac2b
kshiprashetty <kshiprashetty@gmail.com>
2018-01-24 10:38:11 +0530

    caller and applier

---
+++ curry/program5.test.js
@@ -0,0 +1,34 @@
+// const fn = require('./program5')
+
+const callAndApply = require('./program5');
+
+describe('callAndApply', () => {
+  describe('should invoke mockFunction', () => {
+    test('when it is passed as an argument to caller', () => {
+      const mockFunction = jest.fn();
+      callAndApply.caller(null, mockFunction);
+      expect(mockFunction.mock.calls.length).toBe(1);
+    });
+    test('when it is passed as an argument to applier', () => {
+      const mockFunction = jest.fn();
+      callAndApply.applier(null, mockFunction);
+      expect(mockFunction.mock.calls.length).toBe(1);
+    });
+  });
+
+  describe('should invoke mockFunction with the right arguments', () => {
+    test('when it is passed as an argument to caller', () => {
+      const mockFunction = jest.fn();
+      const inputArguments = ['Sahu', 21, 'M'];
+      callAndApply.caller(null, mockFunction, ...inputArguments);
+
+      expect(mockFunction).toHaveBeenCalledWith(...inputArguments);
+    });
+    test('when it is passed as an argument to applier', () => {
+      const mockFunction = jest.fn();
+      const inputArguments = ['Stevens', 34, 'L'];
+      callAndApply.applier(null, mockFunction, inputArguments);
+      expect(mockFunction).toHaveBeenCalledWith(...inputArguments);
+    });
+  });
+});
