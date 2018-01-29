e6aeb8499d8d4d0be8dcc5be18338e8dd7f493c1
kshiprashetty <kshiprashetty@gmail.com>
2018-01-30 04:34:49 +0530

    added test for filtered ls

--- node/filtered-ls.js
+++ node/filtered-ls.js
@@ -1,17 +1,14 @@
 const file = require('fs');
-
-function dir() {
-  const path = process.argv[2];
-  const ext = process.argv[3];
-  file.readdir(path, (err, list) => {
-    list.filter((files) => {
-      const fileext = files.split('.');
-      if (ext === fileext[1]) {
-        console.log(files);
-      } return false;
-    });
+const path = require('path');
+// let result = [];
+function dir(pathname, ext, callback) {
+  // const path = process.argv[2];
+  // const ext = process.argv[3];
+  file.readdir(pathname, 'utf8', (err, list) => {
+    const result = list.filter(filename => path.extname(filename) === `.${ext}`);
+    return callback(result);
   });
 }
 
 module.exports = dir;
-dir();
+// dir();
--- node/filtered-ls.test.js
+++ node/filtered-ls.test.js
@@ -1,10 +1,34 @@
-const filterls = require('./filtered-ls');
+const filters = require('./filtered-ls');
 
-describe('Check for inputs', () => {
-  const path = 'test-file';
-  const ext = 'txt';
-  const answer = ['file1.txt', 'file2.txt', 'zero.txt'];
-  test('Check for directory with all .txt file', () => {
-    expect(filterls('test-file', ext)).toBe(answer);
+describe('Check for files in a directory', () => {
+  test('Check for all files with the specified extention', (done) => {
+    const path = './test-file';
+    const ext = 'txt';
+    const answer = ['file1.txt', 'file2.txt', 'zero.txt'];
+    const callback = (data) => {
+      expect(data).toEqual(answer);
+      done();
+    };
+    filters(path, ext, callback);
+  });
+  test('Check for directory not containing the required type of file', (done) => {
+    const path = './test-file';
+    const ext = 'json';
+    const answer = [];
+    const callback = (data) => {
+      expect(data).toEqual(answer);
+      done();
+    };
+    filters(path, ext, callback);
+  });
+  test('Check for directory containing mixed exts', (done) => {
+    const path = './test-file1';
+    const ext = 'txt';
+    const answer = ['file1.txt'];
+    const callback = (data) => {
+      expect(data).toEqual(answer);
+      done();
+    };
+    filters(path, ext, callback);
   });
 });
