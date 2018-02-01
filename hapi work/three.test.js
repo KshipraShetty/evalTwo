a935da1ccbd617636433a0853e391b7fa2d46bd7
kshiprashetty <kshiprashetty@gmail.com>
2018-02-02 04:47:42 +0530

    added test case for three

---
+++ hapi work/three.test.js
@@ -0,0 +1,21 @@
+const Server = require('./three');
+const fs = require('fs');
+
+describe('Check for rout', () => {
+  test('Check for content', (done) => {
+    Server.inject('/index.html', (response) => {
+      const fileContents = fs.readFileSync('index.html');
+      expect(response.result).toBe(fileContents.toString());
+      // console.log(fileContents.toString());
+      done();
+    });
+  });
+  // test('Check for status', (done) => {
+  //   Server.inject('/handling', (response) => {
+  //     // const fileContents = fs.readFileSync('index.html');
+  //     expect(response.statusCode).toBe(200);
+  //     // console.log(fileContents.toString());
+  //     done();
+  //   });
+  // });
+});
