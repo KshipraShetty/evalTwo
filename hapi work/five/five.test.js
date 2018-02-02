26dd8d813559b22f8396a2ae5576fb2414a31e4c
kshiprashetty <kshiprashetty@gmail.com>
2018-02-02 06:49:45 +0530

    FIVE: updated

--- hapi work/five/five.test.js
+++ hapi work/five/five.test.js
@@ -3,15 +3,15 @@ const fs = require('fs');
 
 describe('Check for route', () => {
   test('Check for statusCode', (done) => {
-    Server.inject('/?name=Handling', (response) => {
+    Server.inject('/?name=the handling', (response) => {
       expect(response.statusCode).toBe(200);
       done();
     });
   });
   test('Check for contents', (done) => {
-    Server.inject('/?name=Handling', (response) => {
+    Server.inject('/?name=the handling', (response) => {
       const fileContents = fs.readFileSync('//Users/kshiprasshetty/techuniversity/hapi work/five/templates/index.html');
-      expect(response.result).toBe(fileContents.toString());
+      expect(response.result).toMatch('Hello the handling');
       done();
     });
   });
