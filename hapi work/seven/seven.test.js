a168b8e1b3fac2a9b3bd252d984ddc56720dc807
kshiprashetty <kshiprashetty@gmail.com>
2018-02-02 09:08:15 +0530

    SEVEN: checking the statuscode

---
+++ hapi work/seven/seven.test.js
@@ -0,0 +1,17 @@
+const Server = require('./seven');
+const helper = require('./helpers/helper');
+
+describe('Check for route', () => {
+  test('Check for statusCode', (done) => {
+    Server.inject('/?name=Hello&suffix=123', (response) => {
+      expect(response.statusCode).toBe(200);
+      done();
+    });
+  });
+  test('Check for contents', (done) => {
+    Server.inject('/?name=all&suffix=welcome', (response) => {
+      expect(response.result).toMatch('Hello all welcome');
+      done();
+    });
+  });
+});
